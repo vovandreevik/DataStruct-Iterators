8f5c18b219e12484fa053e3157190865de14c9f3
Vladimir Andreev <vovandreevik@gmail.com>
2023-04-26 11:15:25 +0300

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,23 @@
+﻿#include <fstream>
+#include "DataStruct.h"
+
+int main() {
+    std::ifstream inputFile("input.txt");
+    if (!inputFile) {
+        std::cerr << "Error opening input file." << std::endl;
+        return -1;
+    }
+
+    std::vector<DataStruct> vec;
+    DataStruct data;
+
+    while (inputFile >> data) {
+        vec.push_back(data);
+    }
+
+    inputFile.close();
+    std::sort(vec.begin(), vec.end());
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+
+    return 0;
+}
